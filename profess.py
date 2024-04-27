51aea894addecd7a62e6a821b3114f99da487fe5
artembutyrin <151617899+artembutyrin@users.noreply.github.com>
2024-04-27 22:18:38 +0700

    Add files via upload

---
+++ profess.py
@@ -0,0 +1,34 @@
+from flask import render_template, Flask, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    title = request.args.get('title', 'Mission to Mars')
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    if "инженер" in prof or "строитель" in prof:
+        type_prof = "Научные симуляторы"
+        image_dir = url_for('static', filename='img/train.jpg')
+    else:
+        type_prof = "Тренажеры"
+        image_dir = url_for('static', filename='img/train1.jpg')
+
+    return render_template("trainings.html", title=type_prof, type_prof=type_prof,
+                           image_dir=image_dir)
+
+
+@app.route('/list_prof/<type_prof>')
+def list_profs(type_prof):
+    list_prof = ["Инженер-исследователь", "Пилот", "Строитель", "Климатолог", "Астрогеолог", "Метеоролог",
+                 "Штурман", "Пилот дронов", "Киберинженер", "Экзобиолог", "Ученый", "Врач"]
+    return render_template("list_prof.html", list_prof=list_prof, types=type_prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
